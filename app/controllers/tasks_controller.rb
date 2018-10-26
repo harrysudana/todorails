86858e6b993572955ecb0cf01a1ed1afb49de390
harrysudana <harrysudana@gmail.com>
2018-10-26 08:24:09 +0800

    add new task method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
 	def index
 
 	end
+
+	def show
+
+	end
+
+	def new
+
+	end
+
+	def create
+		render plain: params[:task].inspect
+	end
+
+	def update
+	
+	end
+
+	def destroy
+	
+	end
 end
