573a8de7399b7ca404c5acbfa3129e5a46fd0ec2
harrysudana <harrysudana@gmail.com>
2018-10-26 08:52:09 +0800

    complete basik Task CRUD

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+	validates :title, presence: true,
+                    length: { minimum: 5 }
+    validates :note, presence: true,
+                    length: { minimum: 5 }
 end
